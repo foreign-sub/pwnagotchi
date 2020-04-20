f2245f41f10dbeab73ce8410110f63327b3430a6
Restyled.io <commits@restyled.io>
2020-04-20 09:32:25 +0000

    Restyled by shellharden

--- builder/data/etc/bash_completion.d/pwnagotchi_completion.sh
+++ builder/data/etc/bash_completion.d/pwnagotchi_completion.sh
@@ -15,15 +15,15 @@ pwnagotchi plugins upgrade -h --help
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     cmd="${COMP_WORDS[@]:0:${#COMP_WORDS[@]}-1}"
-    opt_line="$(grep -m1 "$cmd" <<<$all_options)"
+    opt_line="$(grep -m1 "$cmd" <<<"$all_options")"
     if [[ ${cur} == -* ]] ; then
-        opts="$(echo $opt_line | tr ' ' '\n' | awk '/^ *-/{gsub("[^a-zA-Z0-9-]","",$1);print $1}')"
-        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        opts="$(echo "$opt_line" | tr ' ' '\n' | awk '/^ *-/{gsub("[^a-zA-Z0-9-]","",$1);print $1}')"
+        COMPREPLY=( "$(compgen -W "$opts" -- "$cur")" )
         return 0
     fi
 
-    opts="$(echo $opt_line | grep -Po '{\K[^}]+' | tr ',' '\n')"
-    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    opts="$(echo "$opt_line" | grep -Po '{\K[^}]+' | tr ',' '\n')"
+    COMPREPLY=( "$(compgen -W "$opts" -- "$cur")" )
 }
 
 complete -F _show_complete pwnagotchi
